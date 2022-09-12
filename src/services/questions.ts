7e5cbe83c661e69577cf93a1ded92c4b939b9b00
Elalfy74 <mahmoudelalfy13@gmail.com>
2022-09-12 03:24:00 +0200

    pagination v1

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,11 +1,48 @@
 import { db } from "../firebase-config";
-import { collection, getDocs, getDoc } from "@firebase/firestore";
+import { collection, getDocs, getDoc } from "firebase/firestore";
+import { Loading } from "../data/types";
 import { QuestionType, Tag } from "../components/Question.types";
+import {
+  DocumentData,
+  limit,
+  orderBy,
+  query,
+  QueryDocumentSnapshot,
+  startAfter,
+} from "firebase/firestore";
 
 const questionsCollectionRef = collection(db, "questions");
 
-export const getAllQuestions = async () => {
-  const questionsFromServer = await getDocs(questionsCollectionRef);
+export const getAllQuestions = async (
+  lastDoc: QueryDocumentSnapshot<DocumentData> | null,
+  setLastDoc: React.Dispatch<
+    React.SetStateAction<QueryDocumentSnapshot<DocumentData> | null>
+  >,
+  setLoading: React.Dispatch<React.SetStateAction<Loading>>
+) => {
+  let q;
+
+  if (lastDoc) {
+    q = query(
+      questionsCollectionRef,
+      orderBy("creationTime"),
+      startAfter(lastDoc),
+      limit(6)
+    );
+  } else {
+    q = query(questionsCollectionRef, orderBy("creationTime"), limit(6));
+  }
+
+  setLoading("pending");
+
+  const questionsFromServer = await getDocs(q);
+
+  setLastDoc(questionsFromServer.docs[questionsFromServer.docs.length - 1]);
+
+  if (questionsFromServer.empty) {
+    setLoading("finished");
+    return [];
+  }
 
   const questionsList: QuestionType[] = questionsFromServer.docs.map((doc) => {
     const question = { ...doc.data(), id: doc.id } as QuestionType;
@@ -18,9 +55,9 @@ export const getAllQuestions = async () => {
     });
 
     question.tags = tags;
-    console.log(question.creationTime);
+
     return question;
   });
-
+  setLoading("succeeded");
   return questionsList;
 };
