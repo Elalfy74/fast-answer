9ea06f22c02c590e01a4457a594d00c76b496aea
Mahmoud Abdallah <mah64725@gmail.com>
2022-09-30 02:39:01 +0200

    merge

--- src/layouts/BottomNavigationBar.tsx
+++ src/layouts/BottomNavigationBar.tsx
@@ -8,7 +8,13 @@ import {
   Tag,
   TagOutlined,
 } from '@mui/icons-material';
-import { BottomNavigationAction, Box, Paper } from '@mui/material';
+import {
+  BottomNavigationAction,
+  Box,
+  Paper,
+  SxProps,
+  Theme,
+} from '@mui/material';
 import { NavLink } from 'react-router-dom';
 
 const BottomList = [
@@ -38,28 +44,28 @@ const BottomList = [
   },
 ];
 
+const styles: { [key: string]: SxProps<Theme> } = {
+  paperStyle: {
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    pb: 5,
+    display: {
+      sm: 'none',
+    },
+  },
+  boxStyle: {
+    display: 'flex',
+    pt: 2,
+    justifyContent: 'space-around',
+  },
+};
+
 const BottomNavigationBar = () => {
   return (
-    <Paper
-      sx={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        pb: 5,
-        display: {
-          sm: 'none',
-        },
-      }}
-      elevation={3}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          pt: 2,
-          justifyContent: 'space-around',
-        }}
-      >
+    <Paper sx={styles.paperStyle} elevation={3}>
+      <Box sx={styles.boxStyle}>
         {BottomList.map((link) => (
           <NavLink to={link.path} key={link.label} end>
             {({ isActive }) => (
