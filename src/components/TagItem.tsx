19c6d45793817e5c1c6559f8817dabefc865f70a
Mahmoud Abdallah <mah64725@gmail.com>
2022-09-30 02:16:30 +0200

    make style instead of sx in tagitem

--- src/components/TagItem.tsx
+++ src/components/TagItem.tsx
@@ -1,24 +1,21 @@
-import { Typography } from '@mui/material';
+import { SxProps, Theme, Typography } from '@mui/material';
 
 import { Tag } from '../data/types';
+
+const style: SxProps<Theme> = {
+  p: '1px 3px',
+  fontSize: '13px',
+  borderRadius: '4px',
+  bgcolor: 'hsl(205deg 46% 92%)',
+  color: 'hsl(205deg 47% 42%)',
+};
 
 type TagItemProps = {
   tag: Tag;
 };
-
 const TagItem = ({ tag }: TagItemProps) => {
   return (
-    <Typography
-      component="li"
-      key={tag.id}
-      sx={{
-        p: '1px 3px',
-        fontSize: '13px',
-        borderRadius: '4px',
-        bgcolor: 'hsl(205deg 46% 92%)',
-        color: 'hsl(205deg 47% 42%)',
-      }}
-    >
+    <Typography component="li" key={tag.id} sx={style}>
       {tag.name}
     </Typography>
   );
